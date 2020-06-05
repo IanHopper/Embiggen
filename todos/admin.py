b032a4ec57eed76a6a42bd8a00a7795ee73e1cc2
Ian Hopper <ianhopper@ians-mbp.lan>
2020-06-04 20:45:21 -0500

    initial react commit

--- todos/admin.py
+++ todos/admin.py
@@ -4,7 +4,7 @@ from .models import Todo
 
 class TodoAdmin(admin.ModelAdmin):
   list_display = ('task_name', 'username', 'date_created', 'due_date', 'priority')
-  list_filter = ('username', 'date_created', 'due_date', 'priority')
+  list_filter = ('username', 'priority')
   list_editable = ('due_date', 'priority')
 
 admin.site.register(Todo, TodoAdmin)
