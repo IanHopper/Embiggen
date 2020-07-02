0d683729d0d7bc79fe2f80c00d5deb2215e15c66
Ian Hopper <ianhopper@ians-mbp.lan>
2020-07-02 09:16:45 -0500

    added docker-compose production file and allowed hosts

--- django_todo_app/settings.py
+++ django_todo_app/settings.py
@@ -11,9 +11,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
+ENVIRONMENT = os.environ.get('ENVIRONMENT', default='production')
+
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
