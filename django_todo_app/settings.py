e1fc800bea30cedb6e7ef8ca17e10e477e42987d
Ian Hopper <ianhopper@ians-mbp.lan>
2020-07-02 15:47:37 -0500

    updated urls

--- django_todo_app/settings.py
+++ django_todo_app/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'django_todo_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- django_todo_app/urls.py
+++ django_todo_app/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import index
+
 urlpatterns = [
-    path('', include('users.urls')),
+    path('', index, name='index'),
+    path('api/auth/', include('users.urls')),
     path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
     path('admin-secret-url/', admin.site.urls),
     path('api/', include('apis.urls'))
-    
 ]
