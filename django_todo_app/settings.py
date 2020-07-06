6b1fe68d7bc38e06a2929c1dd772197aa6a28b24
Ian Hopper <ianhopper@ians-mbp.lan>
2020-07-06 14:07:49 -0500

    added frontend static files STATICFILES_DIRS

--- django_todo_app/settings.py
+++ django_todo_app/settings.py
@@ -142,7 +142,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'frontend/static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
