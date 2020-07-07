2117e0da58bb07bf232c725cb37e253fd87b4c4b
Ian Hopper <ianhopper@ians-mbp.lan>
2020-07-06 20:23:34 -0500

    added onClick to login button

--- frontend/src/components/components/accounts/Login.js
+++ frontend/src/components/components/accounts/Login.js
@@ -41,7 +41,7 @@ const Login = () => {
             />
           </div>
           <div className='form-group'>
-            <button type='submit' className='button btn-account'>
+            <button type='submit' className='button btn-account' onClick={(e)=>login(e, loginCredentials.username, loginCredentials.password)}>
               Login
             </button>
           </div>
